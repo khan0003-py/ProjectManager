bc2888d5e01b7c3e3ad941a64cf3d6dd0135f129
Arshad Khan <khanarshad0003@outlook.com>
2024-04-13 10:05:26 +0530

    add component directory

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,20 @@
+import Input from "./Input.jsx";
+export default function NewProject() {
+  return (
+    <div>
+      <menu>
+        <li>
+          <button>Cancel</button>
+        </li>
+        <li>
+          <button>Save</button>
+        </li>
+      </menu>
+      <div>
+        <Input label="Title" type="text"/>
+        <Input label="Description" textarea />
+        <Input label="Due Date" type="date"/>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html","./src/App.jsx","./src/components/ProjectSidebar.jsx","./src/components/NewProject","./src/components/"],
+  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
